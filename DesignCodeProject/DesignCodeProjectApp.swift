b08d198d90ac2e9916e026d44362bb2bb700ae12
Akan <akyshakan@gmail.com>
2024-03-13 18:45:07 +0100

    feat: initial project setup

---
+++ DesignCodeProject/DesignCodeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeProjectApp.swift
+//  DesignCodeProject
+//
+//  Created by Akysh Akan on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DesignCodeProject/Styles.swift
@@ -0,0 +1,34 @@
+//
+//  Styles.swift
+//  DesignCodeProject
+//
+//  Created by Akysh Akan on 13.03.2024.
+//
+
+import SwiftUI
+
+struct StrokeStyle: ViewModifier {
+    var cornerRadius: CGFloat
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    func body(content: Content) -> some View {
+        content.overlay(
+            RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
+                .stroke(
+                    .linearGradient(
+                        colors: [
+                            .white.opacity(colorScheme == .dark ? 0.6 : 0.3),
+                            .black.opacity(colorScheme == .dark ? 0.3 : 0.1)
+                        ],
+                        startPoint: .top, endPoint: .bottom
+                    )
+                )
+                .blendMode(.overlay)
+        )
+    }
+}
+
+extension View {
+    func strokeStyle(cornerRadius: CGFloat = 30) -> some View {
+        modifier(StrokeStyle(cornerRadius: cornerRadius))
+    }
+}

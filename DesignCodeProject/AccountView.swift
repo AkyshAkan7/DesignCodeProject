e3f4be2e7a714d8c585cb9f6f82539e39c92637f
Akan <akyshakan@gmail.com>
2024-03-22 14:03:19 +0100

    feat: added ui views
    added:
    - AccountView
    - BlobView
    - HexagonView

---
+++ DesignCodeProject/AccountView.swift
@@ -0,0 +1,128 @@
+//
+//  AccountView.swift
+//  DesignCodeiOS15
+//
+//  Created by Akysh Akan on 18.03.2024.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    @State var isDeleted = false
+    @State var isPinned = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                profile
+                
+                menu
+                
+                links
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Account")
+        }
+    }
+    
+    // MARK: - Profile View
+    
+    var profile: some View {
+        VStack(spacing: 8) {
+            Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                .symbolVariant(.circle.fill)
+                .font(.system(size: 32))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background(Circle().fill(.ultraThinMaterial))
+                .background(
+                    HexagonView()
+                        .offset(x: -50, y: -100)
+                )
+            Text("Akan Akysh")
+                .font(.title.weight(.semibold))
+            HStack {
+                Image(systemName: "location")
+                    .imageScale(.small)
+                Text("Italy")
+                    .foregroundStyle(.secondary)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+    }
+    
+    // MARK: - Menu View
+    
+    var menu: some View {
+        Section {
+            //first way
+            NavigationLink(destination: ContentView()) {
+                Label("Settings", systemImage: "gear")
+            }
+            //second way
+            NavigationLink { Text("SwiftUI") } label: {
+                Label("Billing", systemImage: "creditcard")
+            }
+            NavigationLink {} label: {
+                Label("Help", systemImage: "questionmark")
+            }
+        }
+        .foregroundStyle(.primary)
+        .listRowSeparatorTint(.blue)
+        .listRowSeparator(.hidden)
+    }
+    
+    // MARK: - Links View
+    
+    var links: some View {
+        Section {
+            if !isDeleted {
+                Link(destination: URL(string: "https://apple.com")!) {
+                    HStack {
+                        Label("Website", systemImage: "house")
+                        Spacer()
+                        Image(systemName: "link")
+                            .foregroundStyle(.secondary)
+                    }
+                }
+                .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                    Button(action: { isDeleted.toggle() }) {
+                        Label("Delete", systemImage: "trash")
+                    }
+                    .tint(.red)
+                    pinButton
+                }
+            }
+            Link(destination: URL(string: "https://youtube.com")!) {
+                HStack {
+                    Label("Youtube", systemImage: "tv")
+                    Spacer()
+                    Image(systemName: "link")
+                        .foregroundStyle(.secondary)
+                }
+            }
+            .swipeActions {
+                pinButton
+            }
+        }
+        .listRowSeparator(.hidden)
+        .foregroundStyle(.primary)
+    }
+    
+    var pinButton: some View {
+        Button { isPinned.toggle()  } label: {
+            if isPinned {
+                Label("Unpin", systemImage: "pin.slash")
+            } else {
+                Label("Pin", systemImage: "pin")
+            }
+        }
+        .tint(isPinned ? .gray : .yellow)
+    }
+}
+
+#Preview {
+    AccountView()
+}

dc65a6ef5ab570ed1ba8f127d43d9b14ced9cdae
Akan <akyshakan@gmail.com>
2024-05-10 15:47:53 +0200

    feat: add focusField to change shadow when active textfield changing

--- DesignCodeProject/Views/SignUpView.swift
+++ DesignCodeProject/Views/SignUpView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct SignUpView: View {
+    enum Field: Hashable {
+        case email
+        case password
+    }
+    
     @State var email = ""
     @State var password = ""
+    @FocusState var focusedField: Field?
+    // y position of circle behind textfield
+    @State var circleY: CGFloat = 120
     
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
@@ -23,9 +31,13 @@ struct SignUpView: View {
                 .keyboardType(.emailAddress)
                 .textInputAutocapitalization(.none)
                 .autocorrectionDisabled(true)
+                .focused($focusedField, equals: .email)
+                .shadow(color: focusedField == .email ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
             SecureField("Password", text: $password)
                 .textContentType(.password)
                 .inputStyle(icon: "lock")
+                .focused($focusedField, equals: .password)
+                .shadow(color: focusedField == .password ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
             Button {} label: {
                 Text("Create an account")
                     .frame(maxWidth: .infinity)
@@ -59,12 +71,28 @@ struct SignUpView: View {
         }
         .padding(20)
         .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .background(
+            Circle()
+                .fill(.blue)
+                .frame(width: 68, height: 68)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .offset(y: circleY)
+        )
         .strokeStyle(cornerRadius: 30)
         .shadow(color: Color("Shadow").opacity(0.2), radius: 30, x: 0, y: 30)
         .padding(20)
         .background(
             Image("Blob 1").offset(x: 200, y: -100)
         )
+        .onChange(of: focusedField) { oldValue, newValue in
+            withAnimation {
+                if newValue == .email {
+                    circleY = 120
+                } else {
+                    circleY = 190
+                }
+            }
+        }
     }
 }
 

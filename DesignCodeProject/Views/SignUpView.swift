6b1e3ed26bd804c875295917043903e2fe88848e
Akan <akyshakan@gmail.com>
2024-05-10 14:46:13 +0200

    feat: added signup view and angular button style

---
+++ DesignCodeProject/Views/SignUpView.swift
@@ -0,0 +1,43 @@
+//
+//  SignUpView.swift
+//  DesignCodeProject
+//
+//  Created by Akysh Akan on 09.05.2024.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("Sign up")
+                .font(.largeTitle).bold()
+            Text("Access 120+ hours of courses, tutorials and livestreams")
+                .font(.headline)
+            Button {} label: {
+                Text("Create an account")
+                    .frame(maxWidth: .infinity)
+            }
+            .font(.headline)
+            .blendMode(.overlay)
+            // button style
+            .buttonStyle(.angular)
+            // button color
+            .tint(.accentColor)
+            // button size
+            .controlSize(.large)
+        }
+        .padding(20)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .strokeStyle(cornerRadius: 30)
+        .shadow(color: Color("Shadow").opacity(0.2), radius: 30, x: 0, y: 30)
+        .padding(20)
+        .background(
+            Image("Blob 1").offset(x: 200, y: -100)
+        )
+    }
+}
+
+#Preview {
+    SignUpView()
+}

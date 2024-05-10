9eec1e46a5498dc873df16c5e0c03a932db9ad76
Akan <akyshakan@gmail.com>
2024-05-10 14:52:58 +0200

    feat: added text with markdown and links

--- DesignCodeProject/Views/SignUpView.swift
+++ DesignCodeProject/Views/SignUpView.swift
@@ -26,6 +26,24 @@ struct SignUpView: View {
             .tint(.accentColor)
             // button size
             .controlSize(.large)
+            
+            Group {
+                Text("By clicking on ")
+                + Text("_Create an account_").foregroundStyle(.primary.opacity(0.7))
+                + Text(", you agree to our **Terms of Service** and **[Privacy Policy](https://designcode.io)**")
+                
+                Divider()
+                
+                HStack {
+                    Text ("Already have an account?")
+                    Button {} label: {
+                        Text("**Sign in**")
+                    }
+                }
+            }
+            .font(.footnote)
+            .foregroundStyle(.secondary)
+            .tint(.secondary)
         }
         .padding(20)
         .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
